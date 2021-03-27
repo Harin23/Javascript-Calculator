23e2e95c008e4e3376bbe80d1e172f899c83c66c
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-27 16:40:58 -0600

    completed layout of the elements

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
+class Pad extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="container" id="buttons">
+                <div className="row">
+                    <div className="col cols-6 cols"><button className="btn btns" id="clear">AC</button></div>
+                    <div className="col cols"><button className="btn btns" id="divide">=</button></div>
+                    <div className="col cols"><button className="btn btns" id="multiply">pow</button></div>
+                </div>
+                <div className="row">
+                    <div className="col cols"><button className="btn btns" id="1">7</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">8</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">9</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">-</button></div>
+                </div>
+                <div className="row">
+                    <div className="col cols"><button className="btn btns" id="1">4</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">5</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">6</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">+</button></div>
+                </div>
+                <div className="row">
+                    <div className="col cols"><button className="btn btns" id="1">1</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">2</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">3</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">*</button></div>
+                </div>
+                <div className="row">
+                    <div className="col cols"><button className="btn btns" id="1">0</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">.</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">root</button></div>
+                    <div className="col cols"><button className="btn btns" id="1">/</button></div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class App extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
         return(
-            <div className="container" id="calculator">
-                
+            <div>
+                <div className="container" id="calculator">
+                    <div className="container" id="display">0</div>
+                    <div className="container" id="pad"><Pad/></div>
+                </div>
+                <div id="signed">-by harin</div>
             </div>
         );
     };
