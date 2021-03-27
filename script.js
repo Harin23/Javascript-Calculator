8d4031521b3bbb5e96eac5fc9f37726adb557a96
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-27 13:28:57 -0600

    project initialized

---
+++ script.js
@@ -0,0 +1,14 @@
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="container" id="calculator">
+                
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
