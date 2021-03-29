d7189b959908c9fa1c9c5ab413e586fbc673c1e0
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-29 16:24:59 -0600

    updated style and html + js partially completed

--- script.js
+++ script.js
@@ -6,51 +6,116 @@ class Pad extends React.Component{
         return(
             <div className="container" id="buttons">
                 <div className="row">
-                    <div className="col cols-6 cols"><button className="btn btns" id="clear">AC</button></div>
-                    <div className="col cols"><button className="btn btns" id="divide">=</button></div>
-                    <div className="col cols"><button className="btn btns" id="multiply">pow</button></div>
+                    <div className="col-9 cols"><button className="btns" onClick={this.props.handleClick} id="clear">AC</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="equals">=</button></div>
                 </div>
                 <div className="row">
-                    <div className="col cols"><button className="btn btns" id="1">7</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">8</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">9</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">-</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="seven">7</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="eight">8</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="nine">9</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="subtract">-</button></div>
                 </div>
                 <div className="row">
-                    <div className="col cols"><button className="btn btns" id="1">4</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">5</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">6</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">+</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="four">4</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="five">5</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="six">6</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="add">+</button></div>
                 </div>
                 <div className="row">
-                    <div className="col cols"><button className="btn btns" id="1">1</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">2</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">3</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">*</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="one">1</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="two">2</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="three">3</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="multiply">*</button></div>
                 </div>
                 <div className="row">
-                    <div className="col cols"><button className="btn btns" id="1">0</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">.</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">root</button></div>
-                    <div className="col cols"><button className="btn btns" id="1">/</button></div>
+                    <div className="col-6 cols"><button className="btns" onClick={this.props.handleClick} id="zero">0</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="decimal">.</button></div>
+                    <div className="col cols"><button className="btns" onClick={this.props.handleClick} id="divide">/</button></div>
                 </div>
             </div>
         );
     }
 }
 
+const DEFUALT_STATE={
+    display:" ",
+    current: "0",
+    decimal: false,
+    evaluated: false,
+    clearCurr: true
+};
+
 class App extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            ...DEFUALT_STATE
+        };
+        this.resetState = this.resetState.bind(this);
+        this.display =this.display.bind(this);
+        this.current = this.current.bind(this);
+    }
+    resetState(){
+        this.setState({
+            ...DEFUALT_STATE
+        });
+    }
+    display(e){
+        let input = e.target.innerText
+        if(input=="AC"){
+            this.resetState()
+        }else if(input=="="){
+            this.setState(state=>({
+                evaluated: true,
+                current: eval(state.display),
+                display: state.display + input + eval(state.display)
+            }));
+        }else if(this.state.evaluated==true){
+            this.resetState();
+            this.current(input);
+        }else{
+            this.current(input);
+        }
+    }
+    current(input){
+        const operator= ["+", "-", "*", "/"];
+        if(input=="."&&this.state.decimal==false){
+            this.setState(state=>({
+                display: state.display + input,
+                current: state.current + input,
+                decimal: true,
+                clearCurr: false
+            }));
+        }else if(input!="."&&operator.indexOf(input)!=-1){
+            this.setState(state=>({
+                display: state.display + input,
+                decimal: false,
+                current: input,
+                clearCurr: true
+            }));
+        }else if(input!="."&&this.state.clearCurr==true){
+            this.setState(state=>({
+                display: state.display + input,
+                current: input
+            }));
+        }else if(input!="."){
+            this.setState(state=>({
+                display: state.display + input,
+                current: state.current + input
+            }));
+        }
     }
     render(){
         return(
             <div>
                 <div className="container" id="calculator">
-                    <div className="container" id="display">0</div>
-                    <div className="container" id="pad"><Pad/></div>
+                    <div className="container" id="full-display">
+                        <div id="expression">{this.state.display}</div>
+                        <div id="display">{this.state.current}</div>
+                    </div>
+                    <div className="container" id="pad"><Pad handleClick={this.display} /></div>
                 </div>
-                <div id="signed">-by harin</div>
+                <div id="signed"><strong>-by harin</strong></div>
             </div>
         );
     };
